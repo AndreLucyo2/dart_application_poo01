ad79ac3ea12f3e660cc3eea6f28fc9e527975f11
Andre Lucyo <andre.lucyo2@gmail.com>
2021-05-01 14:38:08 -0300

    estudo de classe - projeto startup

--- dart_application_poo05/bin/main.dart
+++ dart_application_poo05/bin/main.dart
@@ -1,24 +1,22 @@
-import 'package:dart_application_poo05/dart_application_poo05.dart'
-    as dart_application_poo05;
-
-import 'dart:io';
-
 import 'models/funcionario.dart';
 import 'models/gerente.dart';
 import 'models/pessoa.dart';
 import 'models/presidente.dart';
 
 void main(List<String> arguments) {
+  //instacia funcuionario:
   var funcionario1 = Funcionario(
     pessoa: Pessoa(nome: 'Andre', sobreNome: 'Lucio'),
     dataEntrada: '02/02/2005',
     salario: 15000,
   );
 
+  //instacia Gerente:
   var gerente1 = Gerente(
     pessoa: Pessoa(nome: 'Pedro', sobreNome: 'Cavalho'),
   );
 
+//instacia presidente:
   var presidente1 =
       Presidente(pessoa: Pessoa(nome: 'Lucas', sobreNome: 'Silva'));
 
